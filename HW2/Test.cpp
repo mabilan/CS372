37c10d4506a159b2f7bef0f6e3961935a014d419
Michael Bilan <mabilan@alaska.edu>
2019-03-14 12:03:28 -0800

    Initial Commit for HW2

---
+++ HW2/Test.cpp
@@ -0,0 +1,9 @@
+// Test.cpp
+// Created by Michael Bilan on 3/14/2019.
+// Runs once for all test files.
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE( "1: All test cases reside in other .cpp files (empty)", "[multi-file:1]" ) {
+}
