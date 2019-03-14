062e0035f699d657e636fc42c727b732b63d2f04
Michael Bilan <mabilan@alaska.edu>
2019-03-14 12:16:32 -0800

    Add Add() function - Test for 0 parameter sum 0 case - Corrections from clang

--- HW2/TDDfunctions.h
+++ HW2/TDDfunctions.h
@@ -1,10 +1,15 @@
 // TDDfunctions.h
 // Created by mabilan on 3/14/2019.
 // This file is for the HW2 TDD Kata exercise.
+// This is for TDD Kata from: https://osherove.com/tdd-kata-1
 
 #ifndef HW2_TDDFUNCTIONS_H
 #define HW2_TDDFUNCTIONS_H
 
+#include <string>
 
+int Add(const std::string &param1="", const std::string &param2=""){
+    return 0;
+}
 
 #endif //HW2_TDDFUNCTIONS_H
--- HW2/TDDtests.cpp
+++ HW2/TDDtests.cpp
@@ -1,11 +1,14 @@
 // TDDTests.cpp
 // Created by mabilan on 3/14/2019.
 // This file is a list of tests for a TDD Kata exercise.
+// This is for TDD Kata from: https://osherove.com/tdd-kata-1
 
 #include "catch.hpp"
 #include "TDDfunctions.h"
 
 
 TEST_CASE("Testing for TDD Kata - String Calculator") {
-
+    SECTION("Return sum of 0 string numbers"){
+        REQUIRE(Add("") == 0);
+    }
 }
