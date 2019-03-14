d746ce10d56af48bb91c70e7932f842821309d79
Michael Bilan <mabilan@alaska.edu>
2019-03-14 12:09:34 -0800

    Add testing file and function header file

---
+++ HW2/TDDfunctions.h
@@ -0,0 +1,10 @@
+// TDDfunctions.h
+// Created by mabilan on 3/14/2019.
+// This file is for the HW2 TDD Kata exercise.
+
+#ifndef HW2_TDDFUNCTIONS_H
+#define HW2_TDDFUNCTIONS_H
+
+
+
+#endif //HW2_TDDFUNCTIONS_H
---
+++ HW2/TDDtests.cpp
@@ -0,0 +1,11 @@
+// TDDTests.cpp
+// Created by mabilan on 3/14/2019.
+// This file is a list of tests for a TDD Kata exercise.
+
+#include "catch.hpp"
+#include "TDDfunctions.h"
+
+
+TEST_CASE("Testing for TDD Kata - String Calculator") {
+
+}
